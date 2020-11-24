a9e4a3d5a64e48ca5caae355851e722474054c81
BasicDeveloperVNR <74826138+BasicDeveloperVNR@users.noreply.github.com>
2020-11-24 13:05:17 +0530

    Update FindLongestWord_reduceapproach.js

--- BasicJSProgram/FindLongestWord_reduceapproach.js
+++ BasicJSProgram/FindLongestWord_reduceapproach.js
@@ -1,3 +1,8 @@
+/*
+Reference
+https://www.freecodecamp.org/news/three-ways-to-find-the-longest-word-in-a-string-in-javascript-a2fb04c9757c/
+*/
+
 function findLongestWord(strIn){
 let splStr=strIn.split(' ');
   let longestword = splStr.reduce(function(longest,currentword){
